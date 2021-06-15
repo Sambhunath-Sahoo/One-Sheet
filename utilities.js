729ac61b6731100a45031ec2cca16d87a4d49f85
Sambhu <sambhunathsahoo25@gmail.com>
2021-06-16 03:09:07 +0530

    Add files via upload

---
+++ utilities.js
@@ -0,0 +1,11 @@
+function myOpenFunction() {
+    document.getElementById("open").click();
+}
+
+function colorChangeFunction() {
+    document.getElementById("color").click();
+}
+
+function bgColorChangeFunction() {
+    document.getElementById("bg-color").click();
+}
